8d140180b0d2b8d5c01175d92ded8b520dbbad12
Alex Salgado <alexgsalgado@gmail.com>
2024-03-14 18:05:16 -0300

    feat: add new base components

---
+++ src/app/components/field-input/field-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-field-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './field-input.component.html',
+  styleUrl: './field-input.component.scss'
+})
+export class FieldInputComponent {
+
+}
