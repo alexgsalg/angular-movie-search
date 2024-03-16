35b1f04f650451a28f7efe1b919de0ef51fab1e4
Alex Salgado <alexgsalgado@gmail.com>
2024-03-16 12:55:27 -0300

    feat: add favorite page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    // component: HomeComponent,
     loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule)
-  }
+  },
+  {
+    path: 'favorites',
+    loadChildren: () => import('./modules/favorites/favorites.module').then((m) => m.FavoritesModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
